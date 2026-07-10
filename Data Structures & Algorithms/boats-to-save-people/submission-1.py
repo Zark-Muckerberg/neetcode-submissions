da147c0e217d8aa54e2436e29204935aa71090dd
Zark-Muckerberg <105928626+Zark-Muckerberg@users.noreply.github.com>
2026-07-10 10:42:18 +0800

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        #sort in ascending order
+        people.sort()
+
+        n=len(people)
+        boats = 0
+        i=0
+        j=n-1
+        while i <= j:
+            #pair the lightest with the heaviest
+            weight = people[i]+people[j]
+
+            #if weight exceeds limit, heaviest peson cannot be paired w anyone
+            if weight > limit:
+                boats=boats+1
+                j=j-1
+            else:
+                i=i+1
+                j=j-1
+                boats = boats+1
+        
+        return boats
+
+        
