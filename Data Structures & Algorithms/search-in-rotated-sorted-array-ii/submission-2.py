eb78a70d732d66beeeda67bd05e494a1c0f77336
Zark-Muckerberg <105928626+Zark-Muckerberg@users.noreply.github.com>
2026-07-13 16:10:52 +0800

    Add: search-in-rotated-sorted-array-ii - submission-2

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+
+        l = 0 
+        r = len(nums)-1
+
+        while l <= r:
+            mid = (l+r)//2
+            if nums[mid] == target:
+                return True
+
+            #if l up to mid is sorted
+            if nums[l] < nums[mid]:
+                #left half is sorted, if target is between l and m it must be here
+                if nums[l]<=target and target < nums[mid]:
+                    r = mid-1
+                #target is in the right half
+                else:
+                    l = mid +1
+            #not sorted, see if target lies between mid and r
+            elif nums[l]>nums[mid]:
+                #target lies within right sorted array
+                if target > nums[mid] and target <= nums[r]:
+                    l = mid+1
+                #target is in the left unsorted portion
+                else:
+                    r = mid-1
+            else:
+                l=l+1
+        
+        return False
